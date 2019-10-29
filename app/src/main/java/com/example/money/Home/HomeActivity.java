38dbdd69901119160447da15672c5a34e5556226
vodanh.2901@gmail.com <vodanh.2901@gmail.com>
2019-10-29 13:49:15 +0700

    select month to show transaction

--- app/src/main/java/com/example/money/Home/HomeActivity.java
+++ app/src/main/java/com/example/money/Home/HomeActivity.java
@@ -6,19 +6,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.money.Constants;
 import com.example.money.R;
+import com.example.money.Retrofit.MyService;
+import com.example.money.Retrofit.RetrofitClient;
 import com.example.money.SettingsActivity;
+import com.example.money.models.Transaction;
 import com.google.android.material.navigation.NavigationView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
-
+    private MyService myService;
+    RetrofitClient retrofitClient;
+    TextView title;
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +64,68 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                     new ThisMonthFragment()).commit();
             navigationView.setCheckedItem(R.id.thismonth);
         }
+        //select month
+        imageButton =  findViewById(R.id.action_bar_button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                final int day = calendar.get(Calendar.DAY_OF_MONTH);
+                final  int month = calendar.get(Calendar.MONTH) + 1;
+                final  int year = calendar.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(HomeActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        calendar.set(i,i1,i2);
+                       // Toast.makeText(HomeActivity.this, "year: "+i + "   month "+(i1+1), Toast.LENGTH_SHORT).show();
+                       // SimpleDateFormat simpleDateFormat=  new SimpleDateFormat("yyyy-MM");
+                        String selectYear = String.valueOf(i);
+                        String selectMonth =  String.valueOf((i1+1));
+                        Fragment monthFragment =  new MonthFragment();
+
+                        Bundle data = new Bundle();
+                        data.putString("month",selectMonth);
+                        data.putString("year",selectYear);
+                        monthFragment.setArguments(data);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, monthFragment).commit();
+
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
+
+
+
+
+        Retrofit retrofitClient = RetrofitClient.getInstance();
+        myService = retrofitClient.create(MyService.class);
+        SharedPreferences sharedPreferences =  getSharedPreferences(Constants.SHARED_PREFS,MODE_PRIVATE);
+        String email = sharedPreferences.getString(Constants.EMAIL,null).replace("\"", "");
+        Transaction transaction =  new Transaction(email);
+//        myService.getAll(transaction)
+//                .enqueue(new Callback<List<Transaction>>() {
+//                    @Override
+//                    public void onResponse(Call<List<Transaction>> call, Response<List<Transaction>> response) {
+//                        if(response.isSuccessful()){
+//                            Toast.makeText(HomeActivity.this, "asdasd", Toast.LENGTH_SHORT).show();
+//
+//                            int sum = 0;
+//                            for (Transaction transaction1: response.body()){
+//
+//                                sum = sum + Integer.parseInt(transaction1.getAmount());
+//                            }
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<List<Transaction>> call, Throwable t) {
+//
+//                    }
+//                });
+
+
     }
 
     @Override
--- app/src/main/java/com/example/money/Home/MonthFragment.java
+++ app/src/main/java/com/example/money/Home/MonthFragment.java
@@ -1,8 +1,6 @@
 package com.example.money.Home;
 
 
-import android.app.DatePickerDialog;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -14,9 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.money.Adapter.HomeAdapter;
@@ -26,8 +22,6 @@ import com.example.money.Retrofit.MyService;
 import com.example.money.Retrofit.RetrofitClient;
 import com.example.money.models.Transaction;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.List;
 
 import retrofit2.Call;
@@ -40,14 +34,15 @@ import static android.content.Context.MODE_PRIVATE;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LastMonthFragment extends Fragment {
+public class MonthFragment extends Fragment {
     HomeAdapter homeAdapter;
     private RecyclerView myRecyclerView;
-    RelativeLayout last_month_layout;
+    RelativeLayout month_layout;
     private MyService myService;
     RetrofitClient retrofitClient;
     boolean isDark =  false;
-    public LastMonthFragment() {
+
+    public MonthFragment() {
         // Required empty public constructor
     }
 
@@ -55,35 +50,31 @@ public class LastMonthFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-         View view = inflater.inflate(R.layout.fragment_last_month, container, false);
-        last_month_layout = view.findViewById(R.id.last_month_layout);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_month, container, false);
+        month_layout = view.findViewById(R.id.month_layout);
 
-        final Calendar calendar = Calendar.getInstance();
-        final  int month = calendar.get(Calendar.MONTH);
-        final  int year = calendar.get(Calendar.YEAR);
-
-        getTransactionByMonth( String.valueOf(month), String.valueOf(year));
+        String month =  getArguments().getString("month");
+        String year =  getArguments().getString("year");
+        Toast.makeText(getContext(), ""+month+ " "+year, Toast.LENGTH_SHORT).show();
 
         isDark = getThemeStatePref();
         if (isDark){
-            last_month_layout.setBackgroundColor(getResources().getColor(R.color.black));
+            month_layout.setBackgroundColor(getResources().getColor(R.color.black));
         } else {
-            last_month_layout.setBackgroundColor(getResources().getColor(R.color.white));
+            month_layout.setBackgroundColor(getResources().getColor(R.color.white));
         }
-        myRecyclerView = (RecyclerView) view.findViewById(R.id.rv_lastmonth);
-
+        myRecyclerView = (RecyclerView) view.findViewById(R.id.rv_month);
+        getTransactionByMonth(month,year);
         return view;
     }
-
 
     private void getTransactionByMonth(String month, String year) {
         SharedPreferences sharedPreferences =  getContext().getSharedPreferences(Constants.SHARED_PREFS,MODE_PRIVATE);
         String email = sharedPreferences.getString(Constants.EMAIL,null).replace("\"", "");
         Transaction transaction =  new Transaction(email,month,year);
         Retrofit retrofitClient = RetrofitClient.getInstance();
-
-    //    myRecyclerView = (RecyclerView) this.getView().findViewById(R.id.rv_lastmonth);
-
+        //    myRecyclerView = (RecyclerView) this.getView().findViewById(R.id.rv_lastmonth);
         myService = retrofitClient.create(MyService.class);
         Call<List<Transaction>> call = myService.getAllTransactionsByEmail(transaction);
         call.enqueue(new Callback<List<Transaction>>() {
@@ -98,7 +89,6 @@ public class LastMonthFragment extends Fragment {
                     myRecyclerView.setLayoutManager(layoutManager);
                     myRecyclerView.setAdapter(homeAdapter);
                     Toast.makeText(getActivity(), "success", Toast.LENGTH_SHORT).show();
-
                 }
             }
             @Override
@@ -108,20 +98,11 @@ public class LastMonthFragment extends Fragment {
         });
     }
 
-    private void saveThemeStatePref(boolean isDark) {
-        SharedPreferences preferences = getContext().getSharedPreferences(Constants.SHARED_PREFS, MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("isDark",isDark);
-        editor.apply();
-    }
+
+
     private boolean getThemeStatePref(){
         SharedPreferences preferences = getContext().getSharedPreferences(Constants.SHARED_PREFS, MODE_PRIVATE);
         boolean isDark = preferences.getBoolean(Constants.ISDARK,false);
         return isDark;
     }
 }
-
-
-
-
-
